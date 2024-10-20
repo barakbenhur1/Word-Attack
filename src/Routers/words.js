06217f827f30e253f02231e04f9fab5b44deee45
barakbenhur1 <barakbenhur@gmail.com>
2024-10-20 11:49:38 +0300

    commit3

--- src/Routers/words.js
+++ src/Routers/words.js
@@ -83,7 +83,7 @@ async function scoreboard(email, res) {
 }
 
 async function getMember(diffcultyKey, email) {
-  const dayKey = moment().format("DD/MM/YYYY");
+  const dayKey = moment().tz("Israel/Tel_Aviv").format("DD/MM/YYYY");
 
   const profile = await Profile.findOne({ email: email });
   const languageKey = profile.language;
